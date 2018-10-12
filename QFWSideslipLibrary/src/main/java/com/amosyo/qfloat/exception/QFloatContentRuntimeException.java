5b8fe208c67a11e15d533ca0c4dd29dcace548ed
Qinyu.Wang <qinyu.wang@gometech.com.cn>
2018-10-12 18:09:19 +0800

    update gradle

--- QFWSideslipLibrary/src/main/java/com/amosyo/qfloat/exception/QFloatContentRuntimeException.java
+++ QFWSideslipLibrary/src/main/java/com/amosyo/qfloat/exception/QFloatContentRuntimeException.java
@@ -22,8 +22,4 @@ public class QFloatContentRuntimeException extends RuntimeException {
     public QFloatContentRuntimeException(final Throwable cause) {
         super(cause);
     }
-
-    public QFloatContentRuntimeException(final String message, final Throwable cause, final boolean enableSuppression, final boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
